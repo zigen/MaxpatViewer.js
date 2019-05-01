5b22e3708d1b8fca3c3d15a5d9aa78a5a3b473a1
zigen <hrlclb@gmail.com>
2019-05-01 20:39:27 +0900

    tmp

---
+++ src/MaxObjectRenderers/index.js
@@ -0,0 +1,9 @@
+import newobj from "./newobj";
+import kslider from "./kslider";
+const renders = {
+  newobj,
+  kslider,
+};
+const renderMaxObject = () => {};
+
+export default renderMaxObject();
